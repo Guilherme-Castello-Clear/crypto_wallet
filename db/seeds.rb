9b2095d2233ca902f306da3f50cff9662c8e62e6
Guilherme Castello Clear <guilherme.castello.clear@gmail.com>
2023-05-11 08:49:18 -0300

    Feat: Improved DB Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cadastrando moedas..."
+
+Coin.create!(
+    description: 'bitcoin',
+    acronym: 'BTC',
+    url_image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Bitcoin.svg/640px-Bitcoin.svg.png'
+)
+
+Coin.create!(
+    description: 'Ethereum',
+    acronym: 'ETH',
+    url_image: 'https://1000logos.net/wp-content/uploads/2023/01/Ethereum-logo.png'
+)
+
+Coin.create!(
+    description: 'Dash',
+    acronym: 'DASH',
+    url_image: 'https://media.dash.org/wp-content/uploads/dash-d.png'
+)
+
+puts "Moedas cadastradas com sucesso!"
