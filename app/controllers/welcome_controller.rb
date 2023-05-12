e57affcc043dfd69f24353f02b69632e0487b2c3
Guilherme Castello Clear <guilherme.castello.clear@gmail.com>
2023-05-12 09:21:26 -0300

    Feat: Application translation improved with GET method

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index  
+    cookies[:curso] = "Ruby on Rails [COOKIES]"
+    session[:curso] = "Ruby on Rails [SESSION]"
     @nome = params[:nome]
     @curso = "Rails"
   end
